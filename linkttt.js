42cae69ed9e6ad0527f11596507561f703a2482f
mark91 <mark.mekhayl@gmail.com>
2022-11-22 20:32:46 +1100

    bit of JS updated

--- linkttt.js
+++ linkttt.js
@@ -1,1 +1,27 @@
-var 
+//assigning player Variables
+var linkX = player0
+var zeldaO = player1
+
+//switching between player turns 
+var currentPlayer = play
+if (currentPlayer === linkX){
+    currentPlayer = zeldaO
+} else {
+    currentPlayer = linkX
+}
+
+//assigning DOM to the playing board
+b1 = document.querySelector('#b1').value
+b2 = document.querySelector('#b2').value
+b3 = document.querySelector('#b3').value
+b4 = document.querySelector('#b4').value
+b5 = document.querySelector('#b5').value
+b6 = document.querySelector('#b6').value
+b7 = document.querySelector('#b7').value
+b8 = document.querySelector('#b8').value
+b9 = document.querySelector('#b9').value
+
+
+
+
+
